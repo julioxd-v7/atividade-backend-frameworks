0e0ff70b007b37a357eed47d78bb8cc209981bbc
Julio Vinicius de Almeida Uchoa Correia <01810246@sempreuninassau.com.br>
2026-04-24 11:24:14 -0300

    Subindo atualizacoes do server.js

---
+++ desafio-de-backend-frameworks/node-express/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('o servidor esta rodando');
+});
+
+app.get('/api/teste', (req, res) => {
+    res.json({
+        status: 'ok',
+        mensagem: 'API funcionando direitinho'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
+
+//teste git// teste git
